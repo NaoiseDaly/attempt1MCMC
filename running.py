e82fe181b8ab8139961979fe4d69d781b6750545
ND <121348631@umail.ucc.ie>
2024-11-04 16:14:00 +0000

    add variable for number of mc iterations for reuse accross chains

--- running.py
+++ running.py
@@ -4,17 +4,18 @@ import logging
 logger = logging.getLogger(__name__)
 logging.basicConfig( level=logging.INFO)
 
+mcmc_iterations = 6000
 
-first_chain = basic_metropolis_hastings(-5, 6000)
+first_chain = basic_metropolis_hastings(-5, mcmc_iterations )
 
 simply_plot_the_chain(first_chain, [100, 200, 300] )
 
-first_poisson_chain = poisson_3_MCMC(0, 6000)
+first_poisson_chain = poisson_3_MCMC(0, mcmc_iterations)
 simply_plot_the_chain(first_poisson_chain, fmt_plt="kD")
 plt.bar(*np.unique(first_poisson_chain, return_counts=True))
 plt.show()
 
-second_poisson_chain = poisson_3_MCMC(50, 600)
+second_poisson_chain = poisson_3_MCMC(50, mcmc_iterations)
 
 simply_plot_the_chain(second_poisson_chain, fmt_plt="kD", with_burn_in= [40, 60, 100])
 plt.bar(*np.unique(second_poisson_chain, return_counts=True))
